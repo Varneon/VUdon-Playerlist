32550fd95a00eb4704b168f8fd3f135eb4070c3d
Varneon <me@varneon.com>
2024-03-04 20:30:48 -0800

    Add icon

--- Packages/com.varneon.vudon.playerlist/Editor/PlayerlistEditor.cs
+++ Packages/com.varneon.vudon.playerlist/Editor/PlayerlistEditor.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEditor;
+using UnityEngine;
 using Varneon.VUdon.Editors.Editor;
 
 namespace Varneon.VUdon.Playerlist.Editor
@@ -6,12 +7,16 @@ namespace Varneon.VUdon.Playerlist.Editor
     [CustomEditor(typeof(Playerlist))]
     public class PlayerlistEditor : InspectorBase
     {
+        [SerializeField]
+        private Texture2D headerIcon;
+
         protected override string PersistenceKey => "Varneon/VUdon/Playerlist/Editor/Foldout";
 
         protected override InspectorHeader Header => new InspectorHeaderBuilder()
             .WithTitle("VUdon - Playerlist")
             .WithDescription("UI for displaying information about players in the instance")
             .WithURL("GitHub", "https://github.com/Varneon")
+            .WithIcon(headerIcon)
             .Build();
     }
 }
